1c3a800f4be5a665ac5402943913c75a35857d0a
Nguyen Ngoc Tan <nguyenngoctan44@gmail.com>
2021-09-08 16:11:05 +0700

    Rmeove unsued

--- MatrixSDK/JSONModels/MXJSONModels.m
+++ MatrixSDK/JSONModels/MXJSONModels.m
@@ -2104,8 +2104,7 @@ NSString *const kMXPushRuleScopeStringDevice = @"device";
     MXPreview *mxPreview = [[MXPreview alloc] init];
     if (mxPreview)
     {
-         MXJSONModelSetDictionary(mxPreviewData, JSONDictionary[@"device_data"] ?? "");
-         MXJSONModelSetString(mxPreview.description, JSONDictionary[@"og:description"] ?? "");
+        MXJSONModelSetString(mxPreview.description, JSONDictionary[@"og:description"] ?? "");
         MXJSONModelSetString(mxPreview.url, JSONDictionary[@"og:url"] ?? "");
         MXJSONModelSetString(mxPreview.siteName, JSONDictionary[@"og:site_name"] ?? "");
         MXJSONModelSetString(mxPreview.title, JSONDictionary[@"og:title"] ?? "");
